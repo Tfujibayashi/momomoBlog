938afa3fb214729bc5859b2415a1bdc9a0702f87
momomo1tf <t.fujibayashi314@gmail.com>
2021-08-31 22:29:54 +0900

    add admin page

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,12 +1,64 @@
 <template>
-    <p>admin page</p>
+    <div class="content-list-wrapper">
+        <ul>
+            <li
+                v-for="(markdown, index) in markdownList"
+                v-bind:index="index"
+                v-bind:key="markdown.id"
+            >
+                <a :href="`edit/${markdown[0]}`">
+                    {{markdown[1].summary.slice( 0, 10 )}}
+                </a>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
+import { markdownCollection } from '@/firebase';
+
 export default {
     name: 'Admin',
+    data() {
+        return {
+            markdownList: []
+        }
+    },
+    async beforeCreate () {
+        const markdowns = await markdownCollection.orderBy('time').get();
+        markdowns.forEach(async (doc) => {
+            this.markdownList.push([doc.id, doc.data()]);
+        })
+        console.log(this.markdownList)
+    }
 }
 </script>
 
 <style scoped lang="scss">
+    .content-list-wrapper {
+        font-size: 1.8rem;
+        font-weight: bold;
+        margin: 0 auto;
+        width: 80%;
+
+        ul {
+            padding: 0;
+        }
+
+        ul li, ol li {
+            border-left: solid 6px #1fa67a;
+            border-bottom: solid 2px #dadada;
+            background: whitesmoke;
+            box-sizing: border-box;
+            line-height: 1.5;
+            list-style-type: none!important;
+            margin-bottom: 20px;
+        }
+
+        a {
+            color: #404040;
+            display: block;
+            padding: 0.5em;
+        }
+    }
 </style>
